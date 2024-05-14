6099789d31579f8da466b2eeceb9ec07d36d43ed
Pedro Henrique <pedrouser9@gmail.com>
2024-05-14 16:26:27 -0300

    editando crud

--- src/Components/Products/CardProducts/CardProducts.tsx
+++ src/Components/Products/CardProducts/CardProducts.tsx
@@ -8,7 +8,8 @@ interface CardProductProps {
 }
 
 function CardProduct({ product }: CardProductProps) {
-  const { addProduct, removeProduct } = useContext(AuthContext)
+  const { addProduct, removeProduct, user } = useContext(AuthContext)
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src={product.photo} alt="Product Photo" />
@@ -20,11 +21,17 @@ function CardProduct({ product }: CardProductProps) {
 
       <div className="flex justify-center gap-1 px-6 pt-4 pb-2">
 
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={() => addProduct(product)}>Adicionar</button>
 
+        {user.id === 1 ? <Link to={`/editProduct/${product.id}`} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+          <button>Editar</button>
+        </Link> : <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={() => addProduct(product)}>Adicionar</button> }
+        
+        {user.id === 1 ? <Link to={`/deleteProduct/${product.id}`} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full'>
+          <button>Deletar</button>
+        </Link> :
         <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={() => removeProduct(product.id)}>Remover</button>
+          onClick={() => removeProduct(product.id)}>Remover</button>}
       </div>
     </div>
 
