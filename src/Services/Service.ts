221306e172ea0ff26435316c9b67bab5e43f619b
Pedro Henrique <pedrouser9@gmail.com>
2024-05-16 11:41:42 -0300

    deploy

--- src/Services/Service.ts
+++ src/Services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'https://sollarium-hn4e.onrender.com/'
+export const api = axios.create({
+   baseURL: import.meta.env.VITE_API_URL
 })
 
 export const registerUser = async(url: string, data: Object, setData: Function) => {
